97de2179334ba77e9d2b7b015926bda330a75a1e
Mustafa80baz <112826649+Mustafa80baz@users.noreply.github.com>
2023-05-30 10:19:28 +0300

    Added more info to file

--- src/Work1/HelloClass.java
+++ src/Work1/HelloClass.java
@@ -3,5 +3,6 @@ package Work1;
 public class HelloClass {
     public static void main(String[] args) {
         System.out.println("Work 1 started");
+        System.out.println("Created Git Repository");
     }
 }
