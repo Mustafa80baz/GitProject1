7cf926231574134b6e087a792443577e01abc60e
Mustafa80baz <112826649+Mustafa80baz@users.noreply.github.com>
2023-05-31 10:14:15 +0300

    Mustafa brach ı oluşturuldu

--- src/Work1/HelloClass.java
+++ src/Work1/HelloClass.java
@@ -4,5 +4,6 @@ public class HelloClass {
     public static void main(String[] args) {
         System.out.println("Work 1 started");
         System.out.println("Created Git Repository");
+        System.out.println("Kendi Brachımı oluşturdum");
     }
 }
